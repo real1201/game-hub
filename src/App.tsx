f8ee1054ee6789bba22bb63416994ba46f252d0d
real1201 <janreilgervacio@gmail.com>
2023-06-18 09:44:52 +0800

    implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -34,7 +35,9 @@ function App() {
       }}
     >
       <GridItem area="nav" paddingX={3}>
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FormEvent, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const handleSubmitSearch = (event: FormEvent) => {
+    event.preventDefault();
+    if (ref.current) onSearch(ref.current.value);
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={15} placeholder="Search here..." variant="filled" />
-    </InputGroup>
+    <form onSubmit={(event) => handleSubmitSearch(event)}>
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={15}
+          placeholder="Search here..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
